2bf99022200e155623308c61c3e7733800a39a24
edencomputing <pking@edencomputing.com>
2019-09-30 18:52:50 +0000

    make ts a function, move alta login past discord join

--- index.js
+++ index.js
@@ -2,12 +2,18 @@
 const { WebsocketBot } = require('att-bot-core');
 const { BasicWrapper } = require('att-websockets');
 const Discord = require('discord.js');
+const moment = require('moment');
 
 //Load information from credentials and config
 const { username, password, botToken } = require("./credentials");
 const { targetServers, discordPrefix, discordChannels } = require("./config");
 
 var locations = {};
+
+function ts()
+{ 
+    return "["+ moment().format("h:mm:ss A") +"] " 
+}
 
 //Command list
 const commands = {
@@ -30,45 +36,55 @@ const commands = {
 const logMessage = {
     'PlayerJoined' : ( discord, data ) =>
     {
-        discord.channels.get( discordChannels["PlayerJoined"] ).send( data.user.username +" joined the server" );
-        console.log( data.user.username +" joined the server" );
+        discord.channels.get( discordChannels["PlayerJoined"] ).send( ts() + data.user.username +" joined the server" );
+        console.log( ts() + data.user.username +" joined the server" );
     },
 
     'PlayerLeft' : ( discord, data ) =>
     {
-        discord.channels.get( discordChannels["PlayerLeft"] ).send( data.user.username +" left the server" );
-        console.log( data.user.username +" left the server" );
+        discord.channels.get( discordChannels["PlayerLeft"] ).send( ts() + data.user.username +" left the server" );
+        console.log( ts() + data.user.username +" left the server" );
     },
 
     'PlayerMovedChunk' : ( discord, data ) =>
     {
-        discord.channels.get( discordChannels["PlayerMovedChunk"] ).send( data.player.username +" has moved to "+ data.newChunk ); 
-        console.log( data.player.username +" has moved to chunk "+ data.newChunk );
+        discord.channels.get( discordChannels["PlayerMovedChunk"] ).send( ts() + data.player.username +" has moved to "+ data.newChunk ); 
+        console.log( ts() + data.player.username +" has moved to chunk "+ data.newChunk );
     },
 
     'PlayerKilled' : ( discord, data ) =>
     {
-        discord.channels.get( discordChannels["PlayerKilled"] ).send( data.killerPlayer.username +" has killed "+ data.killedPlayer.username +" in cold blood" );
-        discord.channels.get( discordChannels["PublicPlayerKilled"] ).send( data.killerPlayer.username +" has _slaughtered_ "+ data.killedPlayer.username +" in cold blood" );
-        console.log( "player kill" );
+        console.log( ts() + "player kill" );
         console.log( data );
+        if ( data.killerPlayer != undefined ) 
+        {
+            discord.channels.get( discordChannels["PlayerKilled"] ).send( ts() + data.killerPlayer.username +" has killed "+ data.killedPlayer.username +" in cold blood" );
+            discord.channels.get( discordChannels["PublicPlayerKilled"] ).send( data.killerPlayer.username +" has just _slaughtered_ "+ data.killedPlayer.username +" in cold blood" );
+        } else {
+            if ( data.toolWielder != undefined )
+            {
+                discord.channels.get( discordChannels["PlayerKilled"] ).send( ts() + data.killedPlayer.username +" was killed by: "+ data.toolWielder );
+            } else {
+                discord.channels.get( discordChannels["PlayerKilled"] ).send( ts() + data.killedPlayer.username +" has suddenly died" );
+            }
+        }
     },
 
     'TradeDeckUsed' : ( discord, data ) =>
     {
-        console.log( "trade deck used" );
+        console.log( ts() + "trade deck used" );
         console.log( data );
     },
 
     'CreatureKilled' : ( discord, data ) =>
     {
-        console.log( "creature murdered" );
+        console.log( ts() + "creature murdered" );
         console.log( data );
     },
 
     'CreatureSpawned' : ( discord, data ) =>
     {
-        console.log( "creature has spawned" );
+        console.log( ts() + "creature has spawned" );
         console.log( data );
     }
 
@@ -80,11 +96,10 @@ main();
 
 async function main()
 {
+    console.log( ts() + "bot is starting" );
+
     //Create a new ATT bot
     const bot = new WebsocketBot();
-
-    //Alta Login
-    await bot.login(username, password);
 
     //Connect to discord
     const discord = new Discord.Client();
@@ -116,7 +131,9 @@ async function main()
     });
                     
 
-    //Run the bot.
+    //Alta Login
+    await bot.login(username, password);
+
     //When any of the 'targetServers' are available, a connection is automatically created.
     await bot.run(test => targetServers.includes(test.id), async (server, connection) =>
     {
@@ -128,7 +145,7 @@ async function main()
         // Simple subscriptions
         await wrapper.subscribe("PlayerJoined", data => { logMessage["PlayerJoined"]( discord, data ); })
         await wrapper.subscribe("PlayerLeft", data => { logMessage["PlayerLeft"]( discord, data ); })
-        await wrapper.subscribe("PlayerKilled", data => { logMessage["PlayerKilled"]( discord, data ); })
+        //await wrapper.subscribe("PlayerKilled", data => { logMessage["PlayerKilled"]( discord, data ); })
         await wrapper.subscribe("TradeDeckUsed", data => { logMessage["TradeDeckUsed"]( discord, data ); })
         await wrapper.subscribe("CreatureKilled", data => { logMessage["CreatureKilled"]( discord, data ); })
         await wrapper.subscribe("CreatureSpawned", data => { logMessage["CreatureSpawned"]( discord, data ); });
